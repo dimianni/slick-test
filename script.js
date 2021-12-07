9934adb48b56f2debd136d5e628fbe7e0b382277
Dmytro Anikin <dimianni@users.noreply.github.com>
2021-12-07 14:59:09 -0600

    Upd

--- script.js
+++ script.js
@@ -5,18 +5,26 @@ $(document).ready(function () {
 
 
     const activateSlick = function () {
+
         if ($(window).width() < 713) {
             $('.parent').slick({
                 slidesToShow: 1,
                 slidesToScroll: 1,
+                responsive: [
+                    {
+                        breakpoint: 500,
+                        settings: "unslick"
+                    }
+                ]
             });
+        } else {
+            $('.parent').slick("unslick");
         }
+
     }
 
     $(window).on("resize", activateSlick)
    
-
-    
 
     activateSlick()
 
